f72d6fe9da79d8c0edb56934f487e61a55dd7066
LMBpultd0 <web.lmbpultd.2023@gmail.com>
2023-07-04 21:59:18 +0530

    first updation 4/7/23

---
+++ Home/e-Books/php/login_action.php
@@ -0,0 +1,20 @@
+<?php
+    if(!isset($_POST['submit']))
+{
+  $username=$_POST['Username'];
+  $password=$_POST['password'];
+$conn=mysqli_connect("sql302.infinityfree.com","if0_34542682","EHUWQLsks58UjN","if0_34542682_login");
+$sql="SELECT * from login_data WHERE username='$username' AND password='$password' ";
+$result=mysqli_query($conn,$sql);
+$resultchek=mysqli_num_rows($result);
+if($resultchek>0)
+{
+  header('Location: book1.html');
+}
+else
+{
+echo "login failed";
+ }
+}
+
+?>
