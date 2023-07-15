d301a777a42f7aba38f3b20e2e11c367da7d5056
LMBpultd0 <web.lmbpultd.2023@gmail.com>
2023-07-15 15:46:40 +0530

    sslc update

---
+++ Home/Education/java/button_click.js
@@ -0,0 +1,8 @@
+function myFunctiona() {
+    var x = document.getElementById("sslc");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
