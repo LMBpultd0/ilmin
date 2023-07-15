aa4f088dae234aa40342e585bef74a5e1c741982
LMBpultd0 <web.lmbpultd.2023@gmail.com>
2023-07-15 20:49:53 +0530

    xi

--- Home/Education/java/button_click.js
+++ Home/Education/java/button_click.js
@@ -6,3 +6,23 @@ function myFunctiona() {
       x.style.display = "block";
     }
   }
+
+
+  function myFunctionb() {
+    var x = document.getElementById("XItext");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function myFunctionc() {
+    var x = document.getElementById("XIItext");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
