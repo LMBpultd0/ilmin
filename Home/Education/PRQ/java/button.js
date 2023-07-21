d870602a43dfa6f4f78eb97528d304d45017b648
LMBpultd0 <web.lmbpultd.2023@gmail.com>
2023-07-21 20:17:53 +0530

    hig

---
+++ Home/Education/PRQ/java/button.js
@@ -0,0 +1,17 @@
+function myFunctionbq() {
+    var x = document.getElementById("bcom");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myFunctionmq() {
+    var x = document.getElementById("mcom");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
