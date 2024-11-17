81372d6efd8a2eb0b5724107179bff953634fda8
LMBpultd <web.lmbpultd.2023@gmail.com>
2024-11-18 02:28:23 +0530

    y

---
+++ Body/bc/js/button.js
@@ -0,0 +1,20 @@
+
+function myFunctiona() {
+    var x = document.getElementById("mg");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+
+
+  function myFunctionb() {
+    var x = document.getElementById("apj");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
--- java/home.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("menu-data");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
