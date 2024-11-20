7f2b6ab633a3e71b6eb8c66b9602488065d4de75
LMBpultd <web.lmbpultd.2023@gmail.com>
2024-11-21 00:08:42 +0530

    f

--- Body/bc/js/button.js
+++ Body/bc/js/button.js
@@ -18,3 +18,134 @@ function myFunctiona() {
       x.style.display = "grid";
     }
   }
+
+
+  function myFunctionc() {
+    var x = document.getElementById("chang");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctiond() {
+    var x = document.getElementById("Balachandran-Chullikkad");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+
+  function myFunctione() {
+    var x = document.getElementById("jaj");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+  
+
+  function myFunctionf() {
+    var x = document.getElementById("lt");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctiong() {
+    var x = document.getElementById("mtv");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctionh() {
+    var x = document.getElementById("ov");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctioni() {
+    var x = document.getElementById("pkb");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctionj() {
+    var x = document.getElementById("pc");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctionk() {
+    var x = document.getElementById("ppg");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctionl() {
+    var x = document.getElementById("skp");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctionm() {
+    var x = document.getElementById("sad");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctionn() {
+    var x = document.getElementById("ub");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctiono() {
+    var x = document.getElementById("vmm");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  function myFunctionp() {
+    var x = document.getElementById("vh");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  }
+
+  
