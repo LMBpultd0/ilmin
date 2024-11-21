1b62bc877f40e763f7511925ee5cebffef7334ea
LMBpultd <web.lmbpultd.2023@gmail.com>
2024-11-21 14:03:01 +0530

    poem

---
+++ Body/kavitha/poem/js/button.js
@@ -0,0 +1,160 @@
+
+function myfunctiona() {
+    var x = document.getElementById("aa");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+
+  function myfunctionb() {
+    var x = document.getElementById("bb");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function myfunctionc() {
+    var x = document.getElementById("cc");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctiond() {
+    var x = document.getElementById("dd");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function myfunctione() {
+    var x = document.getElementById("ee");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
+
+  function myfunctionf() {
+    var x = document.getElementById("ff");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctiong() {
+    var x = document.getElementById("gg");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctionh() {
+    var x = document.getElementById("hh");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctioni() {
+    var x = document.getElementById("ii");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctionj() {
+    var x = document.getElementById("jj");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctionk() {
+    var x = document.getElementById("kk");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctionl() {
+    var x = document.getElementById("ll");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctionm() {
+    var x = document.getElementById("mm");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctionn() {
+    var x = document.getElementById("nn");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctiono() {
+    var x = document.getElementById("oo");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function myfunctionp() {
+    var x = document.getElementById("pp");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function myfunctionz() {
+    var x = document.getElementById("zz");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
